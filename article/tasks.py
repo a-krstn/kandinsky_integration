6dd286c380d8d9d55bf593c63ec2c93bfaf7bdb1
Alexandr <art1456@mail.ru>
2024-09-17 14:06:33 +0300

    working version 2

--- article/tasks.py
+++ article/tasks.py
@@ -45,7 +45,7 @@ class Text2ImageAPI:
         data = response.json()
         return data['uuid']
 
-    def check_generation(self, request_id, attempts=10, delay=10):
+    def check_generation(self, request_id, attempts=10, delay=20):
         while attempts > 0:
             response = requests.get(self.URL + 'key/api/v1/text2image/status/' + request_id, headers=self.AUTH_HEADERS)
             data = response.json()
@@ -62,29 +62,20 @@ def get_image(article_pk):
     print(f'Запрос в API: {article.title}')
     api = Text2ImageAPI('https://api-key.fusionbrain.ai/', os.getenv('KANDINSKY_API_KEY'), os.getenv('KANDINSKY_SECRET_KEY'))
     model_id = api.get_model()
-    print(type(model_id), model_id)
     uuid = api.generate(article.body, model_id)
-    print(type(uuid), uuid)
     images = api.check_generation(uuid)
-    print(type(images))
     image_base64 = images[0]
     image_data = base64.b64decode(image_base64)
-
-    # with open(f"{settings.MEDIA_URL}image_{article_pk}.jpg", "wb") as file:
-    # with open(f"{settings.MEDIA_URL}image_{article_pk}.jpg", "w+") as file:
-    # imagead.full_photo.save("NowHiring.jpg", File(open("C:\\NowHiring.jpg", "rb")))
-    # File(open(f"image_{article_pk}.jpg", "rb"))
 
     try:
         with open(f"image_{article_pk}.jpg", "wb") as file:
             file.write(image_data)
-            # article.image = file
-            # article.save(f"image_{article_pk}.jpg", file)
     except:
         with open(f"image_{article_pk}.jpg", "w+") as file:
             file.write(image_data)
-            # article.image = file
     
     article.image.save(f"image_{article_pk}.jpg", File(open(f"image_{article_pk}.jpg", "rb")))
-
     
+    filename = f'/home/user/django/kandinsky_integration/image_{article.pk}.jpg'
+    if os.path.exists(filename):
+        os.remove(filename)
